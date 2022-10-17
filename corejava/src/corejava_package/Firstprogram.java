c4b0dc0c00fe9c7b4f98190f4e1165b98a912ac9
kaviya8338 <113683736+kaviya8338@users.noreply.github.com>
2022-10-17 19:25:23 +0530

    First commit

--- corejava/src/corejava_package/Firstprogram.java
+++ corejava/src/corejava_package/Firstprogram.java
@@ -1,10 +1,24 @@
 package corejava_package;
-
+import java.util.Scanner;
 public class Firstprogram {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner Sc= new Scanner(System.in);
+		String name =Sc.nextLine();
+		int age =Sc.nextInt();
+		int m1 =Sc.nextInt();
+		int m2 =Sc.nextInt();
+		int m3 =Sc.nextInt();
+		int total=m1+m2+m3;
+		float avg= total/3;
+		System.out.println("Name:"+ name);
+		System.out.println("Age:"+ age);
+		System.out.println("M1:"+ m1);
+		System.out.println("M2:"+ m2);
+		System.out.println("M3:"+ m3);
+		System.out.println("total"+total);
+		System.out.println("Average"+ avg);
+		
 	}
 
 }
