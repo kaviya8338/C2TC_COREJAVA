846c069db832bc018266200e9e4389b6e662533a
kaviya8338 <113683736+kaviya8338@users.noreply.github.com>
2022-11-01 12:00:30 +0530

    COMMIT

---
+++ corejava/src/oopsconcept_package/Dress.java
@@ -0,0 +1,29 @@
+package oopsconcept_package;
+
+public class Dress 
+{
+	void type1()
+	{
+		System.out.println("The various designs of dress are");
+		
+	}
+}
+class Indian extends Dress
+{
+	void type2()
+	{
+		System.out.println("saree");
+		
+	}
+}
+class western extends Dress
+{
+	void type3()
+	{
+		System.out.println("jumpsuit");
+		
+	}
+}
+	
+
+
---
+++ corejava/src/oopsconcept_package/Hierarchical.java
@@ -0,0 +1,16 @@
+package oopsconcept_package;
+
+public class Hierarchical {
+
+	public static void main(String[] args) {
+		Dress ob1=new Dress();
+		ob1.type1();
+		western ob2=new western();
+		ob2.type3();
+		Indian ob3=new Indian();
+		ob3.type2();
+		
+
+	}
+
+}
