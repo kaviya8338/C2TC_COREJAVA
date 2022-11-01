2a6096161e24546512e1a01f46429a09f8cfa479
kaviya8338 <113683736+kaviya8338@users.noreply.github.com>
2022-11-01 12:18:02 +0530

    commit

---
+++ corejava/src/org/tns/ifclass/GALLON.java
@@ -0,0 +1,42 @@
+package org.tns.ifclass;
+import java.util.Scanner;
+
+import oopsconcept_package.Dress;
+public class GALLON {
+
+	public static void main(String[] args) {
+	try (Scanner in = new Scanner(System.in))
+	{
+		int gallon = in.nextInt();
+		if (gallon<=45)
+		{
+			System.out.println("No Tax");
+			
+		}
+		else if((gallon>45)&&(gallon<=75))
+		{
+			System.out.println("Tax Amount 475");
+		}
+		else if((gallon>75)&&(gallon<=125))
+		{
+			System.out.println("Tax Amount 750");
+		}
+		else if((gallon>125)&&(gallon<=200))
+		{
+			System.out.println("Tax Amount 1225");
+		}
+		else if((gallon>200)&&(gallon<=350))
+		{
+			System.out.println("Tax Amount 1650");
+		}
+		else 
+		{
+			System.out.println("Tax Amount 2000");
+		}
+	}
+			
+			
+
+	}
+
+}
