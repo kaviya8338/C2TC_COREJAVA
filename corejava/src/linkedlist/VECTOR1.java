8726755de3ef05378a50fe57d046714aba435ccf
kaviya8338 <113683736+kaviya8338@users.noreply.github.com>
2022-11-10 21:02:40 +0530

    commit

---
+++ corejava/src/linkedlist/VECTOR1.java
@@ -0,0 +1,34 @@
+package linkedlist;
+import java.util.*;
+
+public class VECTOR1 {
+
+	public static void main(String[] args) {
+		Vector<String> v = new Vector<String>();
+		int size = v.size();
+		System.out.println("size of the vector Element:"+size);
+		//adding a vector element
+		v.add("chennai");
+		v.add("bangalore");
+		v.add("pune");
+		v.add("vellore");
+		System.out.println("Adding of the vector Element:"+v);
+		int Vectorsize = v.size();
+		System.out.println("VECTOR SIZE:"+v);
+		int capacity = v.capacity();
+		System.out.println("Capcty default:"+size);
+		v.addElement("coimbatore");
+		v.add("mysore");
+		v.add("null");
+		System.out.println("After adding a new Element:"+v);
+		int newsize = v.size();
+		System.out.println("new SIZE:"+newsize);
+		int newcapacity = v.capacity();
+		System.out.println("new capacity:"+newcapacity);
+		
+		
+
+
+	}
+
+}
